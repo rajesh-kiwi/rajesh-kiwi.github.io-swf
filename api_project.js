55c7eeee539894e4ccd1f37e70cb2034593ddbd4
rajesh-kiwi <rajesh.dobhal@kiwitech.com>
2016-06-30 12:53:03 +0530

    swf document

---
+++ api_project.js
@@ -0,0 +1,1 @@
+define({  "name": "SWF - Api Doc",  "version": "1.0.0",  "description": "Services for SWF Application",  "url": "http://localhost:3000",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2016-06-30T07:18:11.387Z",    "url": "http://apidocjs.com",    "version": "0.16.1"  }});
